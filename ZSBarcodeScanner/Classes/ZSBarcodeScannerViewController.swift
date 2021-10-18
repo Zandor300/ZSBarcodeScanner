22c816523ca45ab01d097c21d796265b3cb1aef4
Zandor Smith <info@zsinfo.nl>
2021-10-18 23:48:29 +0200

    Attempt to make scan box align perfectly with barcode.

--- ZSBarcodeScanner/Classes/ZSBarcodeScannerViewController.swift
+++ ZSBarcodeScanner/Classes/ZSBarcodeScannerViewController.swift
@@ -108,6 +108,7 @@ public class ZSBarcodeScannerViewController: UIViewController {
     var outputBarcode: String?
 
     let barcodeFrameView = UIView()
+    var borderLayer: CAShapeLayer?
     var maskLayer: CAShapeLayer?
 
     public override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -141,12 +142,12 @@ public class ZSBarcodeScannerViewController: UIViewController {
                     } else {
                         let alert = UIAlertController(title: self.errorNoCameraPermissionTitle, message: self.errorNoCameraPermissionDescription, preferredStyle: .alert)
                         alert.addAction(UIAlertAction(title: self.errorSettingsButtonText, style: .default, handler: { _ in
-                            if #available(iOS 10.0, *), let settingsUrl = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingsUrl) {
-                                alert.addAction(UIAlertAction(title: self.errorSettingsButtonText, style: .default, handler: { _ in
-                                    UIApplication.shared.open(settingsUrl, options: [:])
-                                    self.dismiss(animated: true, completion: nil)
-                                }))
-                            }
+//                            if #available(iOS 10.0, *), let settingsUrl = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingsUrl) {
+//                                alert.addAction(UIAlertAction(title: self.errorSettingsButtonText, style: .default, handler: { _ in
+//                                    UIApplication.shared.open(settingsUrl, options: [:])
+//                                    self.dismiss(animated: true, completion: nil)
+//                                }))
+//                            }
                             self.dismiss(animated: true, completion: nil)
                         }))
                         alert.addAction(UIAlertAction(title: self.errorOkButtonText, style: .cancel, handler: { _ in
@@ -283,25 +284,152 @@ public class ZSBarcodeScannerViewController: UIViewController {
         }
     }
 
-    func getOverlayCutoutPath(frame: CGRect, rect: CGRect) -> UIBezierPath {
-        let path = UIBezierPath(rect: frame)
-        let pathCutout = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 15, height: 15))
-        path.append(pathCutout)
+    func getPoints(from rect: CGRect) -> [CGPoint] {
+        return [
+            CGPoint(x: rect.minX, y: rect.minY),
+            CGPoint(x: rect.minX, y: rect.maxY),
+            CGPoint(x: rect.maxX, y: rect.maxY),
+            CGPoint(x: rect.maxX, y: rect.minY)
+        ]
+    }
+
+    func getCutoutPath(rect: CGRect) -> CGPath {
+        return getCutoutPath(points: getPoints(from: rect))
+    }
+
+    func distance(_ p1: CGPoint, _ p2: CGPoint) -> CGFloat {
+        return hypot(p1.x - p2.x, p1.y - p2.y)
+    }
+
+    func move(from: CGPoint, to: CGPoint, magnitude: CGFloat) -> CGPoint {
+        let dx = to.x - from.x
+        let dy = to.y - from.y
+        let angle = atan2(dy, dx)
+        let velX = cos(angle) * magnitude
+        let velY = sin(angle) * magnitude
+        return CGPoint(x: from.x + velX, y: from.y + velY)
+    }
+
+    func angle(_ p0: CGPoint, _ p1: CGPoint) -> CGFloat {
+        return atan2(p0.x - p1.x, p0.y - p1.y)
+    }
+
+    func getCutoutPath(points: [CGPoint]) -> CGPath {
+        let path = CGMutablePath()
+
+        let radius: CGFloat = 50
+
+        let pointTopLeft = points[0]
+        let pointBottomLeft = points[1]
+        let pointBottomRight = points[2]
+        let pointTopRight = points[3]
+
+        // Line position - Point position on line
+        let leftTop = move(from: pointTopLeft, to: pointBottomLeft, magnitude: radius)
+        let leftBottom = move(from: pointBottomLeft, to: pointTopLeft, magnitude: radius)
+
+        let bottomLeft = move(from: pointBottomLeft, to: pointBottomRight, magnitude: radius)
+        let bottomRight = move(from: pointBottomRight, to: pointBottomLeft, magnitude: radius)
+
+        let rightBottom = move(from: pointBottomRight, to: pointTopRight, magnitude: radius)
+        let rightTop = move(from: pointTopRight, to: pointBottomRight, magnitude: radius)
+
+        let topRight = move(from: pointTopRight, to: pointTopLeft, magnitude: radius)
+        let topLeft = move(from: pointTopLeft, to: pointTopRight, magnitude: radius)
+
+//        let arcCenterTopLeft = move(from: topLeft, to: bottomLeft, magnitude: radius)
+//        let arcCenterBottomLeft = move(from: leftBottom, to: rightBottom, magnitude: radius)
+//        let arcCenterBottomRight = move(from: bottomRight, to: topRight, magnitude: radius)
+//        let arcCenterTopRight = move(from: rightTop, to: leftTop, magnitude: radius)
+
+        path.move(to: move(from: leftTop, to: leftBottom, magnitude: distance(leftTop, leftBottom) / 2))
+//        path.addLine(to: leftBottom)
+//        path.addArc(center: arcCenterBottomLeft, radius: radius, startAngle: angle(pointTopLeft, pointBottomLeft), endAngle: angle(pointBottomRight, pointBottomLeft), clockwise: true)
+        path.addArc(tangent1End: pointBottomLeft, tangent2End: bottomLeft, radius: radius)
+        path.addLine(to: bottomLeft)
+//        path.addLine(to: bottomRight)
+//        path.addArc(center: arcCenterBottomRight, radius: radius, startAngle: angle(pointBottomRight, pointBottomLeft), endAngle: angle(pointBottomRight, pointTopRight), clockwise: true)
+        path.addArc(tangent1End: pointBottomRight, tangent2End: rightBottom, radius: radius)
+        path.addLine(to: rightBottom)
+//        path.addLine(to: rightTop)
+//        path.addArc(center: arcCenterTopRight, radius: radius, startAngle: angle(pointBottomRight, pointTopRight), endAngle: angle(pointTopLeft, pointTopRight), clockwise: true)
+        path.addArc(tangent1End: pointTopRight, tangent2End: topRight, radius: radius)
+        path.addLine(to: topRight)
+//        path.addLine(to: topLeft)
+//        path.addArc(center: arcCenterTopLeft, radius: radius, startAngle: angle(pointTopLeft, pointTopRight), endAngle: angle(pointTopLeft, pointBottomLeft), clockwise: true)
+        path.addArc(tangent1End: pointTopLeft, tangent2End: leftTop, radius: radius)
+//        path.addLine(to: leftTop)
+//        path.closeSubpath()
+//        path.addArc(tangent1End: p2, tangent2End: move(from: p2, to: p3, magnitude: radius), radius: radius)
+//        path.addLine(to: move(from: p3, to: p2, magnitude: radius))
+//        path.addLine(to: move(from: p0, to: p3, magnitude: radius))
+//        path.addArc(center: move(from: move(from: p0, to: p3, magnitude: radius), to: move(from: p1, to: p2, magnitude: radius), magnitude: radius), radius: radius, startAngle: angle(p0, p1), endAngle: angle(p1, p2), clockwise: false)
+
+//        let p0 = points[0]
+//        let p1 = points[1]
+//        let p2 = points[2]
+//        let p3 = points[3]
+
+//        let radius: CGFloat = 50
+//        path.move(to: move(from: p0, to: p1, magnitude: radius))
+//        path.addLine(to: move(from: p1, to: p0, magnitude: radius))
+//        path.addArc(tangent1End: p1, tangent2End: move(from: p1, to: p2, magnitude: radius), radius: radius)
+//        path.addLine(to: move(from: p2, to: p1, magnitude: radius))
+//        path.addArc(tangent1End: p2, tangent2End: move(from: p2, to: p3, magnitude: radius), radius: radius)
+//        path.addLine(to: move(from: p3, to: p2, magnitude: radius))
+//        path.addArc(tangent1End: p3, tangent2End: move(from: p3, to: p0, magnitude: radius), radius: radius)
+//        path.addLine(to: move(from: p0, to: p3, magnitude: radius))
+//        path.addArc(center: move(from: move(from: p0, to: p3, magnitude: radius), to: move(from: p1, to: p2, magnitude: radius), magnitude: radius), radius: radius, startAngle: angle(p0, p1), endAngle: angle(p1, p2), clockwise: false)
+//        path.addArc(tangent1End: p0, tangent2End: move(from: p0, to: p1, magnitude: radius), radius: radius)
+//        path.addLine(to: move(from: p1, to: p0, magnitude: radius))
+
+//        path.move(to: CGPoint(x: (points[0].x + points[3].x) / 2, y: (points[0].y + points[3].y) / 2))
+//        path.addLine(to: points[0])
+//        path.addLine(to: points[1])
+//        path.addLine(to: points[2])
+//        path.addLine(to: points[3])
+//        path.addLine(to: CGPoint(x: (points[0].x + points[3].x) / 2, y: (points[0].y + points[3].y) / 2))
+//        path.closeSubpath()
+
+//        let radius: CGFloat = 15
+//        path.move(to: CGPoint(x: (points[0].x + points[3].x) / 2, y: (points[0].y + points[3].y) / 2))
+//        path.addArc(tangent1End: points[0], tangent2End: points[1], radius: radius)
+//        path.addArc(tangent1End: points[1], tangent2End: points[2], radius: radius)
+//        path.addArc(tangent1End: points[2], tangent2End: points[3], radius: radius)
+//        path.addArc(tangent1End: points[3], tangent2End: points[0], radius: radius)
+//        path.closeSubpath()
+
+//        let radius: CGFloat = 15
+//        path.move(to: CGPoint(x: points[0].x, y: points[0].y - radius))
+//        path.closeSubpath()
+//        path.addLine(to: points[1])
+//        path.addLine(to: points[2])
+//        path.addLine(to: points[3])
+//        path.addLine(to: points[0])
+
+//        return UIBezierPath(cgPath: path)
+        return path
+    }
+
+    func getOverlayCutoutPath(frame: CGRect, rect: CGRect) -> CGPath {
+        return getOverlayCutoutPath(frame: frame, points: getPoints(from: rect))
+    }
+
+    func getOverlayCutoutPath(frame: CGRect, points: [CGPoint]) -> CGPath {
+//        let path = UIBezierPath(rect: frame)
+//        path.append(getCutoutPath(points: points))
+//        return path
+        let path = CGMutablePath(rect: frame, transform: nil)
+        path.addPath(getCutoutPath(points: points))
         return path
     }
 
     private func setupFrameView() {
         guard showScanningBox else {
-            barcodeFrameView.layer.opacity = 0
             return
         }
-        barcodeFrameView.layer.opacity = 1
-        barcodeFrameView.layer.borderColor = UIColor.white.cgColor
-        barcodeFrameView.layer.borderWidth = 3
-        barcodeFrameView.layer.cornerRadius = 15
+
         let size = min(view.bounds.width, view.bounds.height) - 100
-        barcodeFrameView.frame.size = CGSize(width: size, height: size)
-        barcodeFrameView.center = view.center
 
         let overlay = UIView(frame: view.bounds)
         overlay.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
@@ -311,14 +439,38 @@ public class ZSBarcodeScannerViewController: UIViewController {
             return
         }
         maskLayer.frame = overlay.bounds
+
         maskLayer.fillColor = UIColor.black.cgColor
         maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
 
-        let rect = CGRect(x: overlay.frame.midX - size / 2, y: overlay.frame.midY - size / 2, width: size, height: size)
-        maskLayer.path = getOverlayCutoutPath(frame: overlay.bounds, rect: rect).cgPath
+        let maskRect = CGRect(x: overlay.frame.midX - size / 2, y: overlay.frame.midY - size / 2, width: size, height: size)
+        maskLayer.path = getOverlayCutoutPath(frame: overlay.bounds, rect: maskRect)
         overlay.layer.mask = maskLayer
 
         self.view.addSubview(overlay)
+
+        let border = UIView(frame: view.bounds)
+
+        borderLayer = CAShapeLayer()
+        guard let borderLayer = borderLayer else {
+            return
+        }
+        let borderRect = CGRect(x: border.bounds.midX - size / 2, y: border.bounds.midY - size / 2, width: size, height: size)
+        borderLayer.path = getCutoutPath(rect: borderRect)
+        borderLayer.lineWidth = 10
+        borderLayer.strokeColor = UIColor.white.cgColor
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.frame = border.bounds
+        border.layer.addSublayer(borderLayer)
+
+        self.view.addSubview(border)
+
+//        barcodeFrameView.layer.opacity = 1
+//        barcodeFrameView.layer.borderColor = UIColor.white.cgColor
+//        barcodeFrameView.layer.borderWidth = 3
+//        barcodeFrameView.layer.cornerRadius = 15
+        barcodeFrameView.frame.size = CGSize(width: size, height: size)
+        barcodeFrameView.center = view.center
 
         if !barcodeFrameView.isDescendant(of: view) {
             view.addSubview(barcodeFrameView)
@@ -327,8 +479,28 @@ public class ZSBarcodeScannerViewController: UIViewController {
     }
 
     private func animateScanBox(to rect: CGRect, completion: @escaping () -> Void) {
+        animateScanBox(to: getPoints(from: rect), completion: completion)
+//        UIView.animate(withDuration: scanAnimationDuration, delay: 0, options: .curveEaseInOut) {
+//            self.barcodeFrameView.frame = rect
+//        } completion: { _ in
+//            completion()
+//        }
+//
+//        if let maskLayer = maskLayer {
+//            let animation = CABasicAnimation(keyPath: "path")
+//            animation.duration = scanAnimationDuration
+//            animation.toValue = self.getOverlayCutoutPath(frame: view.bounds, rect: rect).cgPath
+//            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+//            animation.fillMode = CAMediaTimingFillMode.forwards
+//            animation.isRemovedOnCompletion = false
+//            maskLayer.add(animation, forKey: "path")
+//        }
+    }
+
+    private func animateScanBox(to points: [CGPoint], completion: @escaping () -> Void) {
         UIView.animate(withDuration: scanAnimationDuration, delay: 0, options: .curveEaseInOut) {
-            self.barcodeFrameView.frame = rect
+            // TODO: Implement
+            self.barcodeFrameView.frame = self.view.bounds
         } completion: { _ in
             completion()
         }
@@ -336,11 +508,20 @@ public class ZSBarcodeScannerViewController: UIViewController {
         if let maskLayer = maskLayer {
             let animation = CABasicAnimation(keyPath: "path")
             animation.duration = scanAnimationDuration
-            animation.toValue = self.getOverlayCutoutPath(frame: view.bounds, rect: rect).cgPath
+            animation.toValue = self.getOverlayCutoutPath(frame: view.bounds, points: points)
             animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
             animation.fillMode = CAMediaTimingFillMode.forwards
             animation.isRemovedOnCompletion = false
             maskLayer.add(animation, forKey: "path")
+        }
+        if let borderLayer = borderLayer {
+            let animation = CABasicAnimation(keyPath: "path")
+            animation.duration = scanAnimationDuration
+            animation.toValue = self.getCutoutPath(points: points)
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            animation.fillMode = CAMediaTimingFillMode.forwards
+            animation.isRemovedOnCompletion = false
+            borderLayer.add(animation, forKey: "path")
         }
     }
 
@@ -432,15 +613,16 @@ extension ZSBarcodeScannerViewController: AVCaptureMetadataOutputObjectsDelegate
     public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         for metadata in metadataObjects {
             if let readableObject = metadata as? AVMetadataMachineReadableCodeObject, let code = readableObject.stringValue {
-                print(code)
+                print("BarcodeScanner: Scanned '" + code + "'")
                 outputBarcode = code
                 if self.captureSession.isRunning {
                     self.captureSession.stopRunning()
                 }
                 self.currentDevice?.unlockForConfiguration()
                 DispatchQueue.main.async {
-                    if self.showScanningBox, self.scanAnimation, let barCodeObject = self.previewLayer?.transformedMetadataObject(for: metadata) {
-                        self.animateScanBox(to: barCodeObject.bounds) {
+                    if self.showScanningBox, self.scanAnimation, let barCodeObject = self.previewLayer?.transformedMetadataObject(for: readableObject) as? AVMetadataMachineReadableCodeObject {
+                        print(barCodeObject.corners)
+                        self.animateScanBox(to: barCodeObject.corners) {
                             DispatchQueue.main.asyncAfter(deadline: .now() + self.scanPostAnimationDelay) {
                                 self.dismiss(animated: true, completion: {
                                     self.delegate?.barcodeRead(scanner: self, data: code)
